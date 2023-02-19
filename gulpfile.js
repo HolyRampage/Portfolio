4cdda67f400e78ffcd5887404e9af6920050a3cd
Victor <rampageholy@gmail.com>
2023-02-19 12:16:22 +0300

    add promo section

--- gulpfile.js
+++ gulpfile.js
@@ -74,4 +74,4 @@ gulp.task('images', function() {
 		.pipe(browserSync.stream())
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'scripts', 'fonts', 'icons', 'mailer', 'images'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'scripts', 'fonts', 'icons'/* , 'mailer' */, 'images'));
